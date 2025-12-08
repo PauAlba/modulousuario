2c5507514ac85033865f3784e5bb11359acf4393
Edgar Castro <castromendezedgarleonel@gmail.com>
2025-12-08 01:47:29 -0600

    Filtros para la interfaz Admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -4,15 +4,24 @@ from .models import PerfilUsuario, Producto, Compra
 @admin.register(PerfilUsuario)
 class PerfilUsuarioAdmin(admin.ModelAdmin):
     list_display = ('user', 'direccion', 'rol')
+    # filtro lateral para ver "solo Clientes" o "solo Admins"
+    list_filter = ('rol',) 
+    # buscador que busca dentro de la tabla User relacionada
+    search_fields = ('user__username', 'user__email', 'direccion')
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'categoria', 'costo', 'activo', 'creado_en')
-    list_filter = ('categoria', 'activo')
+    # filtro de 'creado_en' para filtrar productos nuevos vs viejos
+    list_filter = ('categoria', 'activo', 'creado_en') 
     search_fields = ('nombre', 'categoria')
+    # barra de navegación por fecha 
+    date_hierarchy = 'creado_en' 
 
 @admin.register(Compra)
 class CompraAdmin(admin.ModelAdmin):
     list_display = ('usuario', 'producto', 'cantidad', 'precio_unitario', 'total', 'fecha')
-    list_filter = ('fecha', 'producto')
+    list_filter = ('fecha', 'producto') # Filtro lateral 
     search_fields = ('usuario__username', 'producto__nombre')
+    # barra de navegación por fecha 
+    date_hierarchy = 'fecha'
